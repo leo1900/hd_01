c104e9a86095f0fbd105339fa6d20cf6a0e9e2f5
liushouyu-dev <shouyu.liu@dragonplus.com>
2021-09-07 19:28:59 +0800

    改变代码结构

---
+++ hd_unity/Assets/Scripts/FrameWork/Editor/AutoMation.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class AutoMation : Editor
+{
+    [MenuItem("Automation/Tools/Build/AndroidBuild", false, 5)]
+    public static void AndroidBuild()
+    {
+        // Build(BuildTarget.Android);
+        Debug.Log("AndroidBuild  Start-----------------------");
+    }
+     
+    [MenuItem("Automation/Tools/Build/iOSBuild", false, 4)]
+    public static void iOSBuild()
+    {
+        Debug.Log("iOSBuild  Start-----------------------");
+    } 
+}
