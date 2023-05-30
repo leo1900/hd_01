fb46cf8994991d7b503d09dc80dac37c43d824aa
liushouyu-dev <shouyu.liu@dragonplus.com>
2023-05-30 11:50:47 +0800

    --bundle加载测试

--- hd_unity/Assets/Scripts/Game/GameMain.cs
+++ hd_unity/Assets/Scripts/Game/GameMain.cs
@@ -1,5 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Resources;
+using FrameWork.Asset;
 using UnityEngine;
 
 public class GameMain : MonoBehaviour
@@ -7,7 +9,9 @@ public class GameMain : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        var gameMain = ResourcesManager.Instance.LoadResource<GameObject>("Prefabs/UI/GameMain",true);
+
+        Instantiate(gameMain);
     }
 
     // Update is called once per frame
---
+++ hd_unity/Assets/Scripts/Test/DownLoadTest.cs
@@ -0,0 +1,13 @@
+using System;
+using FrameWork.Asset;
+using UnityEngine;
+
+namespace Test
+{
+    public class DownLoadTest : MonoBehaviour
+    {
+        private void Awake()
+        {
+        }
+    }
+}
